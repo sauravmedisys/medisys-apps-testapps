2c2129723f82615799daa409a271fd42b0684e3a
Saurav Wahid <saurav@medisys.com.sa>
2016-05-16 12:32:59 +0600

    16MAY2016 12:32 PM

--- app/src/main/java/my/com/medisys/apps/test/webview/WebViewTest.java
+++ app/src/main/java/my/com/medisys/apps/test/webview/WebViewTest.java
@@ -1,8 +1,13 @@
 package my.com.medisys.apps.test.webview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import my.com.medisys.apps.test.R;
 
@@ -13,9 +18,22 @@ public class WebViewTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        String url = "https://www.google.com";
+        String url = "http://176.241.191.36/tas/LogIn.aspx";
         WebView view = (WebView) this.findViewById(R.id.webView);
 
         view.loadUrl(url);
+        view.getSettings().setJavaScriptEnabled(true);
+        // unresponsive site fit within screen
+        view.getSettings().setLoadWithOverviewMode(true);
+        view.getSettings().setUseWideViewPort(true);
+
+    }
+
+    private int getScale(){
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        Double val = new Double(width)/new Double(width);
+        val = val * 100d;
+        return val.intValue();
     }
 }
