791af461f4c8fd9c6473b486545721826fd00263
Saurav Wahid <saurav@medisys.com.sa>
2016-05-16 15:32:26 +0600

    16MAY2016 15:32 PM

--- app/src/main/java/my/com/medisys/apps/test/MainActivity.java
+++ app/src/main/java/my/com/medisys/apps/test/MainActivity.java
@@ -3,18 +3,24 @@ package my.com.medisys.apps.test;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 
 import my.com.medisys.apps.test.activity.ButtonTest;
 import my.com.medisys.apps.test.fragment.FragmentTest;
 import my.com.medisys.apps.test.webview.WebViewTest;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    Button cameraButton;
+    int REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraButton = (Button) this.findViewById(R.id.camera);
+        cameraButton.setOnClickListener(this);
     }
 
     public void activity(View v){
@@ -32,5 +38,14 @@ public class MainActivity extends Activity {
 //        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, WebViewTest.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent,REQUEST_CODE);
+        }
     }
 }
