d2152dce1b214053f0c0b0efd9751d682b0657a8
Saurav Wahid <saurav@medisys.com.sa>
2016-05-16 12:48:15 +0600

    16MAY2016 12:48 PM

--- app/src/main/java/my/com/medisys/apps/test/webview/WebViewTest.java
+++ app/src/main/java/my/com/medisys/apps/test/webview/WebViewTest.java
@@ -12,28 +12,31 @@ import android.widget.Toast;
 import my.com.medisys.apps.test.R;
 
 public class WebViewTest extends AppCompatActivity {
-
+    WebView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
         String url = "http://176.241.191.36/tas/LogIn.aspx";
-        WebView view = (WebView) this.findViewById(R.id.webView);
+        view = (WebView) this.findViewById(R.id.webView);
 
         view.loadUrl(url);
         view.getSettings().setJavaScriptEnabled(true);
         // unresponsive site fit within screen
         view.getSettings().setLoadWithOverviewMode(true);
         view.getSettings().setUseWideViewPort(true);
-
+        // set forward webpage on same activity
+        view.setWebViewClient(new WebViewClient());
     }
 
-    private int getScale(){
-        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        int width = display.getWidth();
-        Double val = new Double(width)/new Double(width);
-        val = val * 100d;
-        return val.intValue();
+    @Override
+    public void onBackPressed() {
+        if (view.canGoBack()){
+            view.goBack();
+        }else {
+            super.onBackPressed();
+        }
     }
+
 }
